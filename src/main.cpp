1e07b7dc85d24d60c0a49ff504b29c338b1e3239
IIIDimaIII <33569757+IIIDimaIII@users.noreply.github.com>
2018-01-27 20:53:45 +0800

    faster

--- src/main.cpp
+++ src/main.cpp
@@ -66,7 +66,7 @@ int main()
           pid.UpdateError(cte);          
           steer_value = pid.TotalError();          
           //throttle    = 0.15 - Kp2 * (pow(std::abs(pid.p_error),1.75));
-          throttle = 0.1;         
+          throttle = 0.2;         
 
           // DEBUG
           std::cout << "CTE: " << cte << " SV: " << steer_value << std::endl;
