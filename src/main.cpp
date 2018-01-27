448fae1e61567e760df2e0ad3aca9a7e94d5bce1
IIIDimaIII <33569757+IIIDimaIII@users.noreply.github.com>
2018-01-27 21:12:48 +0800

    faster

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ int main()
   PID pid;
   PID pid_throttle;
   // TODO: Initialize the pid variable.
-  pid.Init(2,0.0,5.0);
+  pid.Init(2,0.0,6.0);
   int step = 0;
   h.onMessage([&pid,&step](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -65,7 +65,7 @@ int main()
           double Kp2 = 0.05;          
           pid.UpdateError(cte);          
           steer_value = pid.TotalError();          
-          throttle    = 0.2 - Kp2 * (pow(std::abs(pid.p_error),1.75));
+          throttle    = 0.4 - Kp2 * (pow(std::abs(pid.p_error),1.75));
                    
 
           // DEBUG
