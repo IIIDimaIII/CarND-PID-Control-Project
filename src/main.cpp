3bc6649cda4490c16ffdec4d1aa570b7cd1cef02
IIIDimaIII <33569757+IIIDimaIII@users.noreply.github.com>
2018-01-27 20:47:39 +0800

    sharper turns

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ int main()
   PID pid;
   PID pid_throttle;
   // TODO: Initialize the pid variable.
-  pid.Init(1,0.0,0.0);
+  pid.Init(2,0.0,0.0);
   int step = 0;
   h.onMessage([&pid,&step](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
